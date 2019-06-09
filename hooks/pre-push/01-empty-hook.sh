a5bba2baad053ad7ceecdfc9c4ead1680abccb50
Red Huq <redwanhuq@gmail.com>
2019-06-08 22:28:49 -0400

    Add pre-push hook to build docs before a push is complete

--- hooks/pre-push/01-empty-hook.sh
+++ hooks/pre-push/01-empty-hook.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-echo "NOTHING"
+#!/bin/bash
+# This is a pre-push hook that will generate documentation using sphinx before
+# a commit(s) is pushed to a repository.
+make clean
+make html
