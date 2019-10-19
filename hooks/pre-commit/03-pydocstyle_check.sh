a5024657ee4ee3584ebd31f47362baf7b1612e29
Red Huq <redwanhuq@gmail.com>
2019-10-19 00:15:49 -0400

    Update hook docstrings with content from README

--- hooks/pre-commit/03-pydocstyle_check.sh
+++ hooks/pre-commit/03-pydocstyle_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is a pre-commit hook that will warn if any .py file in a commit fails to meet
-# PEP257 standards for docstrings. Note that a commit containing violations will not be
-# rejected.
+# PEP257 standards for docstrings. Note that a commit containing files with violations
+# will not be rejected.
 
 # Iterate over all files in staging area
 for file in `git diff --staged --name-status | sed -e '/^D/ d; /^D/! s/.\s\+//'`
--- hooks/pre-commit/04-black_check.sh
+++ hooks/pre-commit/04-black_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This is a pre-commit hook that will warn if any .py file in a commit has not been
 # processed through the autoformatter black using the argument -S (prevents single
-# quotes from being transformed to double quotes). Note that a commit will never be
-# rejected.
+# quotes from being transformed to double quotes). Note that a commit containing files
+# that should be formatted will not be rejected.
 
 # Iterate over all files in staging area
 for file in `git diff --staged --name-status | sed -e '/^D/ d; /^D/! s/.\s\+//'`
