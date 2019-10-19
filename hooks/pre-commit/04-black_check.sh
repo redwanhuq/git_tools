3df89d80120344f7dabe5551f820b2cc6c9e1fad
Red Huq <redwanhuq@gmail.com>
2019-10-18 23:36:32 -0400

    Add pre-commit hook for using the autoformatter black
    This pre-commit hook will warn if any .py file in a commit has not been processed through the autoformatter black using the argument -S; a commit will never be rejected.

---
+++ hooks/pre-commit/04-black_check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This is a pre-commit hook that will warn if any .py file in a commit has not been
+# processed through the autoformatter black using the argument -S (prevents single
+# quotes from being transformed to double quotes). Note that a commit will never be
+# rejected.
+
+# Iterate over all files in staging area
+for file in `git diff --staged --name-status | sed -e '/^D/ d; /^D/! s/.\s\+//'`
+do
+    # Check whether .py files meet PEP257 standards using pydocstyle linter
+    if [[ $file == *py ]]
+    then
+        OUTPUT=$(black --check -S $file 2>&1)
+        if [[ $OUTPUT == *"reformatted"* ]]
+        then
+        	echo "WARNING: $file is not formatted properly"
+        	echo "Format file by entering: black -S $file"
+        	echo
+        fi
+    fi
+done
+exit 0
