8dceeb6f395eb35723517161f5144505d02d368e
Red Huq <redwanhuq@gmail.com>
2019-10-19 16:51:29 -0400

    Add more detail docstrings of PEP8 linter and black hooks

--- hooks/pre-commit/04-black_check.sh
+++ hooks/pre-commit/04-black_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This is a pre-commit hook that will warn if any .py file in a commit has not been
-# processed through the autoformatter black using the argument -S (prevents single
-# quotes from being transformed to double quotes). Note that a commit containing files
-# that should be formatted will not be rejected.
+# processed through the autoformatter black. The command line argument -S is used with
+# black to ignore its preference for using double quotes in strings. Note that a commit
+# containing files that should be formatted will not be rejected.
 
 # Iterate over all files in staging area
 for file in `git diff --staged --name-status | sed -e '/^D/ d; /^D/! s/.\s\+//'`
