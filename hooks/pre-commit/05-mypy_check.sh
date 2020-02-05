b9bd0de8cf93131c76b20fadf8904abb9d569e99
Red Huq <redwanhuq@gmail.com>
2020-02-04 23:07:58 -0500

    Disable console output if mypy linter run shows no errors

--- hooks/pre-commit/05-mypy_check.sh
+++ hooks/pre-commit/05-mypy_check.sh
@@ -9,8 +9,9 @@ staged_files="$(git diff --staged --name-status | sed -e '/^D/ d; /^D/! s/.\s\+/
 for file in $staged_files; do
   # Check whether file exists and is a python file
   if [[ -f $file && $file == *py ]]; then
-    # Process python file using the linter mypy and record console output
-    output=$(mypy $file)
+    # Process python file using the linter mypy and record console output if errors are
+    # present
+    output=$(mypy $file --no-error-summary)
 
     # Display warning if type hinting errors exist
     if [[ -n "$output" ]]; then
